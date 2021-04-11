3609f1b5212ca0496de55f31d9ee4a3bb15aaf2b
onesterov <https://github.com/malibu87/traineeship.git>
2021-04-11 14:29:07 +0300

    Task 2. Many-to-many relation between Film and Actor

---
+++ custom/myextension/src/training/my/model/ActorModel.java
@@ -0,0 +1,11 @@
+package training.my.model;
+
+import de.hybris.platform.core.model.ItemModel;
+import java.util.Collection;
+
+public class ActorModel extends ItemModel {
+
+  public static final String _TYPECODE = "30275";
+
+  private Collection<FilmModel> _films;
+}
---
+++ custom/myextension/src/training/my/model/FilmModel.java
@@ -0,0 +1,11 @@
+package training.my.model;
+
+import de.hybris.platform.core.model.ItemModel;
+import java.util.List;
+
+public class FilmModel extends ItemModel {
+
+  public static final String _TYPECODE = "30276";
+
+  private List<ActorModel> _actors;
+}
